309c32a8ab66bfccb494ebbef5200fa5c4534632
tomaszd <tomaszdudziec@gmail.com>
2018-08-08 23:43:34 +0200

    added seekbars

--- app/src/main/java/com/example/tomaszek/drfeverworld/MainActivity.java
+++ app/src/main/java/com/example/tomaszek/drfeverworld/MainActivity.java
@@ -1,12 +1,10 @@
 package com.example.tomaszek.drfeverworld;
 
 import android.content.Intent;
-import android.graphics.drawable.Drawable;
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
 import android.view.View;
 import android.widget.Button;
-import android.widget.ImageButton;
 
 public class MainActivity extends AppCompatActivity {
 
--- app/src/main/java/com/example/tomaszek/drfeverworld/ParacetamolActivity.java
+++ app/src/main/java/com/example/tomaszek/drfeverworld/ParacetamolActivity.java
@@ -1,11 +1,9 @@
 package com.example.tomaszek.drfeverworld;
 
 import android.os.Bundle;
-import android.support.design.widget.FloatingActionButton;
-import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
-import android.support.v7.widget.Toolbar;
-import android.view.View;
+import android.widget.SeekBar;
+import android.widget.TextView;
 
 public class ParacetamolActivity extends AppCompatActivity {
 
@@ -14,6 +12,51 @@ public class ParacetamolActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_paracetamol);
 
-    }
+        final SeekBar seekBarWaga = (SeekBar) findViewById(R.id.seekBarWaga);
+        final SeekBar seekBarWiek = (SeekBar) findViewById(R.id.seekBarWiek);
 
+        final TextView textViewWiek = (TextView) findViewById(R.id.textViewWiek);
+        final TextView textViewWaga = (TextView) findViewById(R.id.textViewWaga);
+
+
+        seekBarWaga.setOnSeekBarChangeListener(new SeekBar.OnSeekBarChangeListener() {
+            int progress = 0;
+
+            @Override
+            public void onProgressChanged(SeekBar seekBar, int progresValue, boolean fromUser) {
+                progress = progresValue;
+                textViewWaga.setText(progress + " kg");
+            }
+
+            @Override
+            public void onStartTrackingTouch(SeekBar seekBar) {
+            }
+
+            @Override
+            public void onStopTrackingTouch(SeekBar seekBar) {
+                textViewWaga.setText(progress + " kg");
+            }
+
+        });
+        seekBarWiek.setOnSeekBarChangeListener(new SeekBar.OnSeekBarChangeListener() {
+            int progress = 0;
+
+            @Override
+            public void onProgressChanged(SeekBar seekBar, int progresValue, boolean fromUser) {
+                progress = progresValue;
+                textViewWiek.setText(progress + " years");
+            }
+
+            @Override
+            public void onStartTrackingTouch(SeekBar seekBar) {
+            }
+
+            @Override
+            public void onStopTrackingTouch(SeekBar seekBar) {
+                textViewWiek.setText(progress + " years");
+            }
+        });
+
+    }
 }
+
