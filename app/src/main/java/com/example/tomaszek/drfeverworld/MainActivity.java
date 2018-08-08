c4646b2c423768830240ab4427c72e959dccc67f
Tomasz Dudziec <tomasz.dudziec@redembedded.com>
2018-08-08 14:10:28 +0200

    added icon

--- app/src/main/java/com/example/tomaszek/drfeverworld/MainActivity.java
+++ app/src/main/java/com/example/tomaszek/drfeverworld/MainActivity.java
@@ -1,7 +1,12 @@
 package com.example.tomaszek.drfeverworld;
 
+import android.content.Intent;
+import android.graphics.drawable.Drawable;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.ImageButton;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +14,26 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        final Button buttonParacetamol = (Button) findViewById(R.id.buttonParacetamol);
+        final Button buttonIbuprofen = (Button) findViewById(R.id.buttonIbuprofen);
+
+        buttonParacetamol.setOnClickListener(new Button.OnClickListener() {
+            public void onClick(View v) {
+                Intent myIntent = new Intent(MainActivity.this, ParacetamolActivity.class);
+                MainActivity.this.startActivity(myIntent);
+            }
+        });
+
+        buttonIbuprofen.setOnClickListener(new Button.OnClickListener() {
+            public void onClick(View v) {
+                Intent myIntent = new Intent(MainActivity.this, IbuprofenActivity.class);
+                MainActivity.this.startActivity(myIntent);
+            }
+        });
+
+
     }
+
+
 }
