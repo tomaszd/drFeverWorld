d20cf084dd7d46872fcd67f06fee307e4e542208
tomaszd <tomaszdudziec@gmail.com>
2018-08-10 00:13:39 +0200

    added ibuprofen dosage activity

--- app/src/main/java/com/example/tomaszek/drfeverworld/IbuprofenActivity.java
+++ app/src/main/java/com/example/tomaszek/drfeverworld/IbuprofenActivity.java
@@ -1,11 +1,19 @@
 package com.example.tomaszek.drfeverworld;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.Button;
+import android.widget.ImageView;
 import android.widget.SeekBar;
 import android.widget.TextView;
 
+import static com.example.tomaszek.drfeverworld.R.drawable.age;
+
 public class IbuprofenActivity extends AppCompatActivity {
+    int age = 0;
+    int weight = 0;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -17,7 +25,7 @@ public class IbuprofenActivity extends AppCompatActivity {
 
         final TextView textViewWiek = (TextView) findViewById(R.id.textViewWiek);
         final TextView textViewWaga = (TextView) findViewById(R.id.textViewWaga);
-
+        final ImageView imageViewCalc = (ImageView) findViewById(R.id.imageViewCalc);
 
         seekBarWaga.setOnSeekBarChangeListener(new SeekBar.OnSeekBarChangeListener() {
             int progress = 0;
@@ -25,6 +33,7 @@ public class IbuprofenActivity extends AppCompatActivity {
             @Override
             public void onProgressChanged(SeekBar seekBar, int progresValue, boolean fromUser) {
                 progress = progresValue;
+                weight = progresValue;
                 textViewWaga.setText(progress + "\nkg");
             }
 
@@ -44,6 +53,7 @@ public class IbuprofenActivity extends AppCompatActivity {
             @Override
             public void onProgressChanged(SeekBar seekBar, int progresValue, boolean fromUser) {
                 progress = progresValue;
+                age = progresValue;
                 textViewWiek.setText(progress + "\nyears");
             }
 
@@ -56,6 +66,16 @@ public class IbuprofenActivity extends AppCompatActivity {
                 textViewWiek.setText(progress + "\nyears");
             }
         });
+
+        imageViewCalc.setOnClickListener(new Button.OnClickListener() {
+            public void onClick(View v) {
+                Intent myIntent = new Intent(IbuprofenActivity.this, IbuprofenDosageActivity.class);
+                myIntent.putExtra("age", age);
+                myIntent.putExtra("weight", weight);
+                IbuprofenActivity.this.startActivity(myIntent);
+            }
+        });
+
     }
 
 }
--- app/src/main/java/com/example/tomaszek/drfeverworld/ParacetamolActivity.java
+++ app/src/main/java/com/example/tomaszek/drfeverworld/ParacetamolActivity.java
@@ -1,11 +1,17 @@
 package com.example.tomaszek.drfeverworld;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.Button;
+import android.widget.ImageView;
 import android.widget.SeekBar;
 import android.widget.TextView;
 
 public class ParacetamolActivity extends AppCompatActivity {
+    int age = 0;
+    int weight = 0;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -18,6 +24,8 @@ public class ParacetamolActivity extends AppCompatActivity {
         final TextView textViewWiek = (TextView) findViewById(R.id.textViewWiek);
         final TextView textViewWaga = (TextView) findViewById(R.id.textViewWaga);
 
+        final ImageView imageViewCalc = (ImageView) findViewById(R.id.imageViewCalc);
+
 
         seekBarWaga.setOnSeekBarChangeListener(new SeekBar.OnSeekBarChangeListener() {
             int progress = 0;
@@ -25,6 +33,7 @@ public class ParacetamolActivity extends AppCompatActivity {
             @Override
             public void onProgressChanged(SeekBar seekBar, int progresValue, boolean fromUser) {
                 progress = progresValue;
+                weight = progresValue;
                 textViewWaga.setText(progress + "\nkg");
             }
 
@@ -44,6 +53,7 @@ public class ParacetamolActivity extends AppCompatActivity {
             @Override
             public void onProgressChanged(SeekBar seekBar, int progresValue, boolean fromUser) {
                 progress = progresValue;
+                age = progresValue;
                 textViewWiek.setText(progress + "\nyears");
             }
 
@@ -54,6 +64,15 @@ public class ParacetamolActivity extends AppCompatActivity {
             @Override
             public void onStopTrackingTouch(SeekBar seekBar) {
                 textViewWiek.setText(progress + "\nyears");
+            }
+        });
+
+        imageViewCalc.setOnClickListener(new Button.OnClickListener() {
+            public void onClick(View v) {
+                Intent myIntent = new Intent(ParacetamolActivity.this, ParacetamolDosageActivity.class);
+                myIntent.putExtra("age", age);
+                myIntent.putExtra("weight", weight);
+                ParacetamolActivity.this.startActivity(myIntent);
             }
         });
 
