335d32d818ff42b6bf69cd58aff14fa3a605d30a
tomaszd <tomaszdudziec@gmail.com>
2018-08-10 23:28:57 +0200

    added calc activity

--- app/src/main/java/com/example/tomaszek/drfeverworld/ParacetamolConcentration.java
+++ app/src/main/java/com/example/tomaszek/drfeverworld/ParacetamolConcentration.java
@@ -1,7 +1,11 @@
 package com.example.tomaszek.drfeverworld;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.ImageView;
 
 public class ParacetamolConcentration extends AppCompatActivity {
 
@@ -9,5 +13,16 @@ public class ParacetamolConcentration extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_paracetamol_concentration);
+        final ImageView imageViewCalc = (ImageView) findViewById(R.id.imageViewCalc);
+
+
+        imageViewCalc.setOnClickListener(new Button.OnClickListener() {
+            public void onClick(View v) {
+                Intent myIntent = new Intent(ParacetamolConcentration.this, ParacetamolCalcActivity.class);
+                ParacetamolConcentration.this.startActivity(myIntent);
+            }
+        });
+
+
     }
 }
