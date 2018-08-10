f8643f9b1497a82d57561189635895f113052b20
tomaszd <tomaszdudziec@gmail.com>
2018-08-10 22:36:37 +0200

    good background

---
+++ app/src/main/java/com/example/tomaszek/drfeverworld/ParacetamolConcentration.java
@@ -0,0 +1,13 @@
+package com.example.tomaszek.drfeverworld;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class ParacetamolConcentration extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_paracetamol_concentration);
+    }
+}
