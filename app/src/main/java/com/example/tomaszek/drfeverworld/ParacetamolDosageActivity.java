7827847296cfff21ed623cb6b3edcb96032ce9b4
tomaszd <tomaszdudziec@gmail.com>
2018-08-10 23:02:15 +0200

    good background

--- app/src/main/java/com/example/tomaszek/drfeverworld/ParacetamolDosageActivity.java
+++ app/src/main/java/com/example/tomaszek/drfeverworld/ParacetamolDosageActivity.java
@@ -1,11 +1,14 @@
 package com.example.tomaszek.drfeverworld;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.view.View;
+import android.widget.Button;
+import android.widget.ImageView;
 
 public class ParacetamolDosageActivity extends AppCompatActivity {
 
@@ -13,6 +16,16 @@ public class ParacetamolDosageActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_paracetamol_dosage);
+
+        final ImageView imageViewSirop = (ImageView) findViewById(R.id.imageViewSirop);
+
+        imageViewSirop.setOnClickListener(new Button.OnClickListener() {
+            public void onClick(View v) {
+                Intent myIntent = new Intent(ParacetamolDosageActivity.this, ParacetamolConcentration.class);
+                ParacetamolDosageActivity.this.startActivity(myIntent);
+            }
+        });
+
 
     }
 
