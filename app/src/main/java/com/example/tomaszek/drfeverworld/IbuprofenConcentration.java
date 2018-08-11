ca2a8e39a6157f4a6958fc9464e73c3065b41e90
tomaszd <tomaszdudziec@gmail.com>
2018-08-11 23:18:09 +0200

    Added Ibuprofen concentration

--- app/src/main/java/com/example/tomaszek/drfeverworld/IbuprofenConcentration.java
+++ app/src/main/java/com/example/tomaszek/drfeverworld/IbuprofenConcentration.java
@@ -1,7 +1,11 @@
 package com.example.tomaszek.drfeverworld;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.ImageView;
 
 public class IbuprofenConcentration extends AppCompatActivity {
 
@@ -9,5 +13,13 @@ public class IbuprofenConcentration extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_ibuprofen_concentration);
+        final ImageView imageViewCalc = (ImageView) findViewById(R.id.imageViewCalc);
+
+        imageViewCalc.setOnClickListener(new Button.OnClickListener() {
+            public void onClick(View v) {
+                Intent myIntent = new Intent(IbuprofenConcentration.this, IbuprofenCalcActivity.class);
+                IbuprofenConcentration.this.startActivity(myIntent);
+            }
+        });
     }
 }
