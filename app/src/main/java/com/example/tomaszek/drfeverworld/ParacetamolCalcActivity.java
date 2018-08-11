3261131f85bcadec9c23de98007dd2409ed04cb3
tomaszd <tomaszdudziec@gmail.com>
2018-08-11 23:47:26 +0200

    import

--- app/src/main/java/com/example/tomaszek/drfeverworld/ParacetamolCalcActivity.java
+++ app/src/main/java/com/example/tomaszek/drfeverworld/ParacetamolCalcActivity.java
@@ -1,7 +1,9 @@
 package com.example.tomaszek.drfeverworld;
 
+import android.content.SharedPreferences;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.widget.TextView;
 
 public class ParacetamolCalcActivity extends AppCompatActivity {
 
@@ -9,5 +11,26 @@ public class ParacetamolCalcActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_paracetamol_calc);
+
+        int age = 0;
+        int weight = 0;
+        String Mg = "";
+        String Ml = "";
+
+
+        final TextView text_view_id = (TextView) findViewById(R.id.text_view_id);
+
+        SharedPreferences pref = getApplicationContext().getSharedPreferences("MyPref", 0); // 0 - for private mode
+        age = pref.getInt("age", 0); // getting Integer
+        weight = pref.getInt("weight", 0); // getting Integer
+        Mg = pref.getString("Mg", "buba"); // getting Integer
+        Ml = pref.getString("Ml", "buba"); // getting Integer
+
+
+        text_view_id.setText(" Age : " + String.valueOf(age) +
+                "\nWeight : " + String.valueOf(weight) +
+                "\nMg : " + String.valueOf(Mg) +
+                "\nMl : " + String.valueOf(Ml));
+
     }
 }
