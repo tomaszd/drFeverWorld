f8cb3f297ce903cd07460251f2d0e606a48f6ff2
tomaszd <tomaszdudziec@gmail.com>
2018-08-13 23:35:33 +0200

    added activity for parac+ supp + pill

--- app/src/main/java/com/example/tomaszek/drfeverworld/IbuprofenCalcActivity.java
+++ app/src/main/java/com/example/tomaszek/drfeverworld/IbuprofenCalcActivity.java
@@ -4,6 +4,9 @@ import android.content.Intent;
 import android.content.SharedPreferences;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.Button;
+import android.widget.ImageView;
 import android.widget.TextView;
 
 public class IbuprofenCalcActivity extends AppCompatActivity {
@@ -24,6 +27,15 @@ public class IbuprofenCalcActivity extends AppCompatActivity {
 
 
         final TextView text_view_id = (TextView) findViewById(R.id.text_view_id);
+
+        final ImageView imageViewHome = (ImageView) findViewById(R.id.imageViewHome);
+
+        imageViewHome.setOnClickListener(new Button.OnClickListener() {
+            public void onClick(View v) {
+                Intent myIntent = new Intent(IbuprofenCalcActivity.this, MainActivity.class);
+                IbuprofenCalcActivity.this.startActivity(myIntent);
+            }
+        });
 
         SharedPreferences pref = getApplicationContext().getSharedPreferences("MyPref", 0); // 0 - for private mode
         age = pref.getInt("age", 0); // getting Integer
