d399f7a772464862407e79b163d775a7c8dd592d
tomaszd <tomaszdudziec@gmail.com>
2018-08-20 22:34:06 +0200

    added nice button

--- app/src/main/java/com/example/tomaszek/drfeverworld/InformationActivity.java
+++ app/src/main/java/com/example/tomaszek/drfeverworld/InformationActivity.java
@@ -1,7 +1,11 @@
 package com.example.tomaszek.drfeverworld;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.ImageView;
 
 public class InformationActivity extends AppCompatActivity {
 
@@ -9,5 +13,15 @@ public class InformationActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_information);
+        final ImageView imageViewOK = (ImageView) findViewById(R.id.imageViewOK);
+
+        imageViewOK.setOnClickListener(new Button.OnClickListener() {
+            public void onClick(View v) {
+                Intent myIntent = new Intent(InformationActivity.this, MainActivity.class);
+                InformationActivity.this.startActivity(myIntent);
+            }
+        });
+
+
     }
 }
