712767c7be1cb6c2dbcb290bc99e89c3702f10d6
Tomasz Dudziec <tomasz.dudziec@redembedded.com>
2018-08-22 10:43:13 +0200

    added tutorial

--- app/src/main/java/pl/doktornutria/tomaszek/drfeverworld/MainActivity.java
+++ app/src/main/java/pl/doktornutria/tomaszek/drfeverworld/MainActivity.java
@@ -5,6 +5,7 @@ import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.view.View;
 import android.widget.Button;
+import android.widget.ImageView;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -14,6 +15,7 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
         final Button buttonParacetamol = (Button) findViewById(R.id.buttonParacetamol);
         final Button buttonIbuprofen = (Button) findViewById(R.id.buttonIbuprofen);
+        final ImageView imageViewInfo = (ImageView) findViewById(R.id.imageViewInfo);
 
         buttonParacetamol.setOnClickListener(new Button.OnClickListener() {
             public void onClick(View v) {
@@ -28,5 +30,15 @@ public class MainActivity extends AppCompatActivity {
                 MainActivity.this.startActivity(myIntent);
             }
         });
+
+        imageViewInfo.setOnClickListener(new Button.OnClickListener() {
+            public void onClick(View v) {
+                Intent myIntent = new Intent(MainActivity.this, WelcomeActivity.class);
+                myIntent.putExtra("explicitCall", true); //Optional parameters
+                MainActivity.this.startActivity(myIntent);
+            }
+        });
+
+
     }
 }
