eb1370300745013557e8e37bcf7e17d34a12c287
Tomasz Dudziec <tomasz.dudziec@redembedded.com>
2019-05-20 14:51:57 +0200

    Added better dosage for ibuprofen /paracetamol

--- app/src/main/java/pl/doktornutria/tomaszek/drfeverworld/IbuprofenCalculationSuppActivity.java
+++ app/src/main/java/pl/doktornutria/tomaszek/drfeverworld/IbuprofenCalculationSuppActivity.java
@@ -29,7 +29,7 @@ public class IbuprofenCalculationSuppActivity extends AppCompatActivity {
 
         imageViewCalc.setOnClickListener(new Button.OnClickListener() {
             public void onClick(View v) {
-                Intent myIntent = new Intent(IbuprofenCalculationSuppActivity.this, ParacetamolCalcActivity.class);
+                Intent myIntent = new Intent(IbuprofenCalculationSuppActivity.this, IbuprofenCalcActivity.class);
                 myIntent.putExtra("ibuprofen_way_of_giving", "supp");
                 String Mg = editTextMg.getText().toString();
                 editor.putInt("IbuprofenMgSupp", Integer.valueOf(Mg));
